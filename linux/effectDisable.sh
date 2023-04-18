f59adec6ce0e57cec964c3f361e2d35d4ab40f79
sohaibaltaib <55866933+sohaibaltaib@users.noreply.github.com>
2023-04-18 13:26:24 -0600

    init commit

---
+++ linux/effectDisable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo 0 > /sys/devices/platform/ff204100.wahWahEffectProcessor/enable
+
---
+++ linux/effectEnable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo 1 > /sys/devices/platform/ff204100.wahWahEffectProcessor/enable
+
